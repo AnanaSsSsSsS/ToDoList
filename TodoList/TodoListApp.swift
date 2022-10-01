8b89fb78b014b7c6382a9e6861b6f867e6e29eda
AnanaSsSsSsS <elias.gold@bk.ru>
2022-10-01 20:46:33 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by macbook on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
